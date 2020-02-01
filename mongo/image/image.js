08211914e757513830fd5e3c26cc0c6fec995bd7
John Marshall <jjmarsha@usc.edu>
2020-02-01 04:26:33 -0800

    api

--- mongo/image/image.js
+++ mongo/image/image.js
@@ -1,9 +1,19 @@
 
 const express = require("express");
-const meth = express.Router()
+const router = express.Router()
+const Image = require("./schema")
 
-meth.post("/", (req, res) => {
-    console.log(req.body)
+router.post("/", (req, res) => {
+    const data = {
+        cloud_url: req.body.cloud_url,
+        image_url: req.body.image_url
+    };
+
+    let image = new Image(data);
+    image
+        .save()
+        .then(image => res.status(200).json(image))
+        .catch(err => res.status(500).send(err));
 })
 
-module.exports = meth
+module.exports = router
