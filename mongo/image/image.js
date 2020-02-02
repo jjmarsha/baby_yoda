f236ad0d4be4d33f459acb14c13fd920c72e81e4
elliebababa <ellie24.huang@gmail.com>
2020-02-02 04:30:10 -0800

    babyyoda

--- mongo/image/image.js
+++ mongo/image/image.js
@@ -3,31 +3,49 @@ const router = express.Router();
 const Image = require("./schema");
 const vision = require('@google-cloud/vision');
 const client = new vision.ImageAnnotatorClient();
+var fs = require('fs'),request = require('request');
 
 
-router.post("/", async(req, res) => {
+var download = function(uri, filename, callback){
+  request.head(uri, function(err, res, body){
+    console.log('content-type:', res.headers['content-type']);
+    console.log('content-length:', res.headers['content-length']);
+    request(uri).pipe(fs.createWriteStream(filename)).on('close', callback);
+  });
+};
+
+
+
+
+router.post("/", (req, res) => {
     const data = {
         cloud_url: req.body.cloud_url,
         image_url: req.body.image_url
     };
 
-    const [result] = await client.faceDetection(`gs://my-project-test-cv.appspot.com/${data.image_url}`);
-    const faces = result.faceAnnotations;
-    if(faces.length > 0) {
-        console.log("uploaded")
-        let image = new Image(data);
-        image
-            .save()
-            .then(image => res.status(200).json(image))
-            .catch(err => res.status(500).send(err));
-    } else {
-        console.log("No face");
-    }
+    console.log(data);
 
+    download(data.cloud_url, 'tmp.jpg', async () => {
+        const [result] = await client.faceDetection("tmp.jpg");
+        const faces = result.faceAnnotations;
+        if(faces.length > 0) {
+                    let image = new Image(data);
+                    image
+                        .save()
+                        .then(image => res.status(200).json(image))
+                        .catch(err => res.status(500).send(err));
+                    res.status(200).send("success")
+                } else {
+                    console.log("No face");
+                    console.log(faces)
+                    res.status(502).send("failure")
+                }
+    });
 })
 
 router.get("/", (req, res) => {
     Image.find({}, (err, data) => {
+        console.log(data)
         var map = [];
         data.forEach(info => {
             map.push(info);
