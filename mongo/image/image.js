c5a935bbbd96ed8c42b05dbbbbc3f970c250f4a2
John Marshall <jjmarsha@usc.edu>
2020-02-02 03:03:24 -0800

    main

--- mongo/image/image.js
+++ mongo/image/image.js
@@ -11,11 +11,14 @@ router.post("/", async(req, res) => {
         image_url: req.body.image_url
     };
 
+
         let image = new Image(data);
         image
             .save()
             .then(image => res.status(200).json(image))
             .catch(err => res.status(500).send(err));
+
+            res.send("accessed")
 })
 
 router.get("/", (req, res) => {
