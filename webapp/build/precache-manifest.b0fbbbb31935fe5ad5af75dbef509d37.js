ad1817dc9beef639641f5edb100a1c05bb301e66
John Marshall <jjmarsha@usc.edu>
2020-02-02 04:57:20 -0800

    stff

---
+++ webapp/build/precache-manifest.b0fbbbb31935fe5ad5af75dbef509d37.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8ed7fbbf61b20bf7d1164828ef4117a3",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2730b0f1057565ed8c87",
+    "url": "/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "d437f417a541639ea1dc",
+    "url": "/static/css/main.e1a9743d.chunk.css"
+  },
+  {
+    "revision": "2730b0f1057565ed8c87",
+    "url": "/static/js/2.08c85f6d.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "/static/js/2.08c85f6d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d437f417a541639ea1dc",
+    "url": "/static/js/main.ad6ad91e.chunk.js"
+  },
+  {
+    "revision": "85e6a98b86ef3993b97a",
+    "url": "/static/js/runtime-main.5de74a9c.js"
+  }
+]);
