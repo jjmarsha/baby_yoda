5e4f2fb7967bc130252e0a7b3dfee8a01b1844cf
John Marshall <jjmarsha@usc.edu>
2020-02-02 06:31:54 -0800

    design

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,11 +1,13 @@
 import React from "react";
-import Offenders from "./components/Offenders"
+import Offenders from "./components/Offenders";
+import Yoda from "./static/yoda.png";
 
 const App = () => {
     return(
         <div >
-            <header>
-                <h1>USC Guardian</h1>
+            <header style={{textAlign: "center"}}>
+                <img src={Yoda} style={{position: "relative", left: "50px", top: "-20px", width: "1000px"}}/>
+                <div style={{fontSize: "100px", paddingBottom: "0px ", position: "relative", top: "-100px", fontFamily: "Arial"}}>Guardian of USC</div>
             </header>
             <Offenders/>
         </div>
--- webapp/src/components/Offenders.js
+++ webapp/src/components/Offenders.js
@@ -1,8 +1,9 @@
 import React from "react";
 import axios from "axios";
 import { Card } from 'react-bootstrap';
-// const url = "localhost:3001/image";
-const url = "/api/image";
+
+// const url = "/api/image";
+const url = "http://34.70.122.5:8100/api/image"
 
 export default class Offenders extends React.Component {
     constructor(props) {
@@ -28,7 +29,7 @@ export default class Offenders extends React.Component {
                 <div style={{display: "flex", justifyContent: "space-around", flexWrap: "wrap"}}>
                     {this.state.offenders.map((value) => {
                         return (
-                            <Card style={{ width: '18rem', display: "inline-block" }}>
+                            <Card style={{ width: '20rem', display: "inline-block" }}>
                                 <Card.Img variant="top" src={value.cloud_url} />
                                 <Card.Body>
                                     <Card.Title>Suspect</Card.Title>
